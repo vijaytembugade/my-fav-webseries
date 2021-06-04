13bee37f6ca1842f0abd238731ec99aaf4350127
vijaytembugade <vijaytembugade21@gmail.com>
2021-06-04 18:28:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-5hotgy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-5hotgy)
---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const webSeries = {
+  Netflix: [
+    { name: 'Money Heist', imdb: '8.3' },
+    { name: 'Peaky Blinders', imdb: '8.8' },
+    { name: 'Vikings ', imdb: '8.5' },
+    { name: 'Sacred Games ', imdb: '8.6' }
+  ],
+  'Amazon Prime': [
+    { name: 'Mirzapur ', imdb: '8.4' },
+    { name: 'Family Man', imdb: '6.8' }
+  ],
+  'HBO Max': [
+    { name: 'Game of Thrones', imdb: '9.3' },
+    { name: 'Silicon Valley ', imdb: '8.5' }
+  ]
+};
+
+const images = {
+  Netflix: 'https://img.icons8.com/ios/50/000000/netflix--v2.png',
+  'Amazon Prime':
+    'https://img.icons8.com/wired/64/000000/amazon-prime-video.png',
+  'HBO Max': 'https://img.icons8.com/ios-filled/50/000000/hbo.png'
+};
+
+export default function App() {
+  const [platforms, setPlatforms] = useState('Netflix');
+
+  const handleCLick = key => {
+    setPlatforms(key);
+  };
+  return (
+    <div className="container">
+      <h2> 🎬 My favorite web series and their platforms</h2>
+      <span>Choose from below</span>
+      <div>
+        {Object.keys(webSeries).map((key, index) => {
+          return (
+            <span
+              className="platform"
+              key={key}
+              onClick={() => handleCLick(key)}
+            >
+              <img src={images[key]} />
+            </span>
+          );
+        })}
+      </div>
+      <hr />
+
+      <div className="content-block">
+        {webSeries[platforms].map(platform => {
+          return (
+            <div key={platform.name} className="content">
+              {platform.name}
+              <br />
+              <span> IMDB rating :{platform.imdb}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
